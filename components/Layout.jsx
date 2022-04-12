3c13d51a986a15009e08e5cc3ff190afa3f0bbd0
Tom Jehle <jehletom@hotmail.com>
2022-04-13 01:39:38 +0200

    custom font and mobile nav tweaks

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,6 +7,12 @@ const Layout = ({ children }) => {
   return (
     <>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;700;800&display=swap"
+          rel="stylesheet"
+        />
         <title>Next.js E-commerce</title>
       </Head>
       <header>
