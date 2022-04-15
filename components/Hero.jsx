3eceb00675ac208591b111fd9da387df05a246b0
Tom Jehle <jehletom@hotmail.com>
2022-04-15 18:45:57 +0200

    added hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import heroImg from "../images/1hero.png";
 
 import styles from "../styles/Hero.module.css";
@@ -10,9 +11,12 @@ const Hero = () => {
       <div className={styles.container}>
         <Image className={styles.heroImg} src={heroImg} width={"1920px"} height={"850px"} alt="Hero image" />
         <div>
-          <h1 className={styles.heroText}>
-            <span className={styles.heroTextSpan}> Tests </span>
-          </h1>
+          <section className={styles.heroText}>
+            <h3 className={styles.hero1}>Enjoy Summer {new Date().getUTCFullYear()}</h3>
+            <h3 className={styles.hero2}>#newarrivals</h3>
+            <h3 className={styles.hero3}>We make your styles</h3>
+            <Link href="/">Explore now</Link>
+          </section>
         </div>
       </div>
     </>
