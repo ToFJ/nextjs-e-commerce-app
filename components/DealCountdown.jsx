e5b8c527140b3c0adc17ef60c7a97d5b7e4e4777
Tom Jehle <jehletom@hotmail.com>
2022-04-18 22:03:38 +0200

    implemented deal of the week countdown

--- components/DealCountdown.jsx
+++ components/DealCountdown.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import img from "../images/discountimg.png";
@@ -5,6 +6,28 @@ import img from "../images/discountimg.png";
 import styles from "../styles/DealCountdown.module.css";
 
 const DealCountdown = () => {
+  const [days, setDays] = useState(new Date().getDate());
+  const [hours, setHours] = useState(new Date().getHours());
+  const [minutes, setMinutes] = useState(new Date().getMinutes());
+  const [seconds, setSeconds] = useState(new Date().getSeconds());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance =
+        new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 3).getTime() - now;
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      setDays(days);
+      setHours(hours);
+      setMinutes(minutes);
+      setSeconds(seconds);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <section className={styles.section}>
       <div className={styles.container}>
@@ -15,19 +38,19 @@ const DealCountdown = () => {
           <h2 className={styles.heading}>Deal Of The Week</h2>
           <div className={styles.countdown}>
             <div className={styles.daysMedia}>
-              <span className={styles.countdown__time}>00</span>
+              <span className={styles.countdown__time}>{days}</span>
               <span className={styles.countdown__label}>Days</span>
             </div>
             <div>
-              <span className={styles.countdown__time}>00</span>
+              <span className={styles.countdown__time}>{hours}</span>
               <span className={styles.countdown__label}>Hours</span>
             </div>
             <div>
-              <span className={styles.countdown__time}>00</span>
+              <span className={styles.countdown__time}>{minutes}</span>
               <span className={styles.countdown__label}>Minutes</span>
             </div>
             <div>
-              <span className={styles.countdown__time}>00</span>
+              <span className={styles.countdown__time}>{seconds}</span>
               <span className={styles.countdown__label}>Seconds</span>
             </div>
           </div>
